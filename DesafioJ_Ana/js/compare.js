a71d85804e6020e4623b3c94c57020f47ee7f111
LauraPereiradev <ana.l.pereira@ba.estudante.senai.br>
2025-10-20 16:04:13 -0300

    Projeto final
    Updated Car class constructor and comparison logic.

--- DesafioJ_Ana/js/compare.js
+++ DesafioJ_Ana/js/compare.js
@@ -6,6 +6,18 @@ class Car {
    
 
     constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
+        this.nome = nome;
+        this.preco = preco;
+        this.alturaCacamba = alturaCacamba;
+        this.alturaVeiculo = alturaVeiculo;
+        this.alturaSolo = alturaSolo;
+        this.capacidadeCarga = capacidadeCarga;
+        this.motor = motor;
+        this.potencia = potencia;
+        this.volumeCacamba = volumeCacamba;
+        this.roda = roda;
+        this.image = image;
+
        
     }
 } 
@@ -20,20 +32,27 @@ function GetCarArrPosition(arr, carClass) {
 }
 
 function SetCarToCompare(el, carClass) {
-   
-    if(carClass instanceof Car){       
-        if(el.checked){
-                
-            
-        } else {
-          
-        } 
-    } else {
-        throw "You need set a Car Class";
+
+    if(el.checked && carArr.length >=2){
+    alert("Selecione apenas 2 carros"); 
+    el.checked=false;
+    return;
     }
+   
+    // if(carClass instanceof Car){       
+        if(el.checked){
+            carArr.push(carClass);
+        } else {
+            const index = GetCarArrPosition(carArr,carClass);
+            carArr.splice(index,1)
+        } 
+    // } else {
+    //     throw "You need set a Car Class";
+    // }
 }
 
 function ShowCompare() {
+    
     if(carArr.length < 2) {
         alert("Precisa marcar 2 carros para apresentar a comparação");
         return;
@@ -48,5 +67,42 @@ function HideCompare(){
 }
 
 function UpdateCompareTable() {
-    
+
+    const compare0 = carArr[0];
+    const compare1 = carArr[1];
+
+    document.getElementById("compare_modelo_0").innerHTML = compare0.nome;
+    document.getElementById("compare_modelo_1").innerHTML = compare1.nome;
+
+    document.getElementById("compare_preco_0").innerHTML = compare0.preco;
+    document.getElementById("compare_preco_1").innerHTML = compare1.preco;
+
+    document.getElementById("compare_alturacacamba_0").innerHTML = compare0.alturaCacamba;
+    document.getElementById("compare_alturacacamba_1").innerHTML = compare1.alturaCacamba;
+
+    document.getElementById("compare_alturaveiculo_0").innerHTML = compare0.alturaVeiculo;
+    document.getElementById("compare_alturaveiculo_1").innerHTML = compare1.alturaVeiculo;
+
+    document.getElementById("compare_alturasolo_0").innerHTML = compare0.alturaSolo;
+    document.getElementById("compare_alturasolo_1").innerHTML = compare1.alturaSolo;
+
+    document.getElementById("compare_capacidadecarga_0").innerHTML = compare0.capacidadeCarga;
+    document.getElementById("compare_capacidadecarga_1").innerHTML = compare1.capacidadeCarga;
+
+    document.getElementById("compare_motor_0").innerHTML = compare0.motor;
+    document.getElementById("compare_motor_1").innerHTML = compare1.motor;
+
+    document.getElementById("compare_potencia_0").innerHTML = compare0.potencia;
+    document.getElementById("compare_potencia_1").innerHTML = compare1.potencia;
+
+    document.getElementById("compare_volumecacamba_0").innerHTML = compare0.volumeCacamba;
+    document.getElementById("compare_volumecacamba_1").innerHTML = compare1.volumeCacamba;
+
+    document.getElementById("compare_roda_0").innerHTML = compare0.roda;
+    document.getElementById("compare_roda_1").innerHTML = compare1.roda;
+
+    document.getElementById("compare_image_0").innerHTML = ` <img src="${compare0.image}" alt="">`;
+    document.getElementById("compare_image_1").innerHTML = ` <img src="${compare1.image}" alt="">`;
+   
+
 }
